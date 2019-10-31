fba58138ed2b4539f70a7f14e1cdd121c8350712
lucrecioSantos <53883786+lucrecioSantos@users.noreply.github.com>
2019-10-30 21:55:06 -0300

    Update AtividadeAc2.java

--- src/main/java/br/com/drummond/resources/AtividadeAc2.java
+++ src/main/java/br/com/drummond/resources/AtividadeAc2.java
@@ -1,10 +1,12 @@
 package br.com.drummond.resources;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class AtividadeAc2 {
 	
